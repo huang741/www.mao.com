5b9277c909df2c94380c689bfd9d2c9b48c9b9c7
huang741 <709704302@qq.com>
2015-12-16 17:36:28 +0800

    增加内容

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -9,3 +9,28 @@ var registerShow=function(){
      $(".register .type a").addClass("active");
  })
 }
+/*企业注册内容展示与隐藏*/
+
+/*开发市场内容展示与隐藏*/
+var marketShow=function(){
+   $("#head li").mouseenter(function(){
+       $("#head li").removeClass("active");
+       $(this).addClass("active");
+       var index = $(this).index();
+       $(".projectlist").removeClass("active");
+       $("#con"+index).addClass("active");
+   })
+}
+/*开发市场内容展示与隐藏*/
+
+/*技术服务市场内容展示与隐藏*/
+var skillShow=function(){
+    $("#shillHead li").click(function(){
+        $("#shillHead li").removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $(".t-detail .con").removeClass("active");
+        $("#skillCon"+index).addClass("active");
+    })
+}
+/*技术服务市场内容展示与隐藏*/
