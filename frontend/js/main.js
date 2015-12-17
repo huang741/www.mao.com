09737c3f228f2e4928e82a8501ec1626e38aec9f
huang741 <709704302@qq.com>
2015-12-17 19:08:45 +0800

    增加产品页面和支付页面

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -34,3 +34,26 @@ var skillShow=function(){
     })
 }
 /*技术服务市场内容展示与隐藏*/
+
+/*产品列表内容展示与隐藏*/
+var productShow=function(){
+    $(".product-side .p-side-ul li").mouseenter(function(){
+        var index = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        $(this).closest(".panel").find(".p-side-con").removeClass("active");
+        $(this).closest(".panel").find(".panel-body > ul").eq(index).addClass("active");
+    });
+}
+/*产品列表内容展示与隐藏*/
+
+/*立即支付选择平台*/
+var paySelect=function(){
+    $("#terraceUl li").click(function(){
+        $("#terraceUl li i").removeClass("fa-check-circle");
+        $("#terraceUl li i").addClass("fa-circle-o");
+        $(this).children("i").removeClass("fa-circle-o");
+        $(this).children("i").addClass("fa-check-circle");
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+}
+/*立即支付选择平台*/
